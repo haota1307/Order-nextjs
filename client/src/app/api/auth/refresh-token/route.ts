e0451c7b0b986963a9257c5d9f1f2021f9c54a67
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-06-27 20:15:54 +0700

    Tạo refresh token route handler

---
+++ client/src/app/api/auth/refresh-token/route.ts
@@ -0,0 +1,33 @@
+import authApiRequest from '@/app/apiRequests/auth'
+import { decode } from 'jsonwebtoken'
+import { cookies } from 'next/headers'
+
+export async function POST(request: Request) {
+  const cookieStore = cookies()
+  const refreshToken = cookieStore.get('refreshToken')?.value
+  if (!refreshToken) {
+    return Response.json({ message: 'Không lấy được refresh token' }, { status: 401 })
+  }
+  try {
+    const { payload } = await authApiRequest.sRefreshToken({ refreshToken })
+    const decodeAccessToken = decode(payload.data.accessToken) as { exp: number }
+    const decodeRefreshToken = decode(payload.data.refreshToken) as { exp: number }
+    cookieStore.set('accessToken', payload.data.accessToken, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      expires: new Date(decodeAccessToken.exp * 1000),
+    })
+    cookieStore.set('refreshToken', payload.data.refreshToken, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      expires: new Date(decodeRefreshToken.exp * 1000),
+    })
+    return Response.json(payload)
+  } catch (error: any) {
+    Response.json({ message: error.message ?? 'Có lỗi xảy ra' }, { status: 401 })
+  }
+}
--- client/src/app/apiRequests/auth.ts
+++ client/src/app/apiRequests/auth.ts
@@ -1,5 +1,11 @@
 import http from '@/lib/http'
-import { LoginBodyType, LoginResType, LogoutBodyType } from '@/schemaValidations/auth.schema'
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
+} from '@/schemaValidations/auth.schema'
 
 const authApiRequest = {
   // Server login
@@ -18,6 +24,10 @@ const authApiRequest = {
     ),
   // AT & RT tự động gửi qua cookie
   logout: () => http.post('/api/auth/logout', null, { baseUrl: '' }),
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>('/auth/refresh-token', body),
+  refreshToken: () =>
+    http.post<RefreshTokenResType>('/api/auth/refresh-token', null, { baseUrl: '' }),
 }
 
 export default authApiRequest
