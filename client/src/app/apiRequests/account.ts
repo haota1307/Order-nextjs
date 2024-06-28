31f7962dfbfe1718701a81468e717ef37c267f53
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-06-28 19:30:29 +0700

    Khai báo API cho accounts

--- client/src/app/apiRequests/account.ts
+++ client/src/app/apiRequests/account.ts
@@ -1,7 +1,10 @@
 import http from '@/lib/http'
 import {
+  AccountListResType,
   AccountResType,
   ChangePasswordBodyType,
+  CreateEmployeeAccountBodyType,
+  UpdateEmployeeAccountBodyType,
   UpdateMeBodyType,
 } from '@/schemaValidations/account.schema'
 
@@ -16,6 +19,13 @@ const accountApiRequest = {
   updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>('/accounts/me', body),
   changePassword: (body: ChangePasswordBodyType) =>
     http.put<AccountResType>('/accounts/change-password', body),
+  list: () => http.get<AccountListResType>('/accounts'),
+  addEmployee: (body: CreateEmployeeAccountBodyType) =>
+    http.post<AccountResType>('/accounts', body),
+  updatEmployee: (id: number, body: UpdateEmployeeAccountBodyType) =>
+    http.put<AccountResType>(`/accounts/detail/${id}`, body),
+  getEmployee: (id: number) => http.get<AccountResType>(`/accounts/detail/${id}`),
+  deleteEmployee: (id: number) => http.delete<AccountResType>(`/accounts/detail/${id}`),
 }
 
 export default accountApiRequest
