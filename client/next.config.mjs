1e2fee441da3ab62927b807ee78688f4b6385d03
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-08-06 09:28:04 +0700

    update: Tích hợp i18n không routing

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
+import createNextIntlPlugin from 'next-intl/plugin'
+
 /** @type {import('next').NextConfig} */
+
+const withNextIntl = createNextIntlPlugin()
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -14,4 +18,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withNextIntl(nextConfig)
--- client/src/app/(public)/page.tsx
+++ client/src/app/(public)/page.tsx
@@ -1,10 +1,13 @@
 import dishApiRequest from '@/apiRequests/dish'
 import { formatCurrency } from '@/lib/utils'
 import { DishListResType } from '@/schemaValidations/dish.schema'
+import { getTranslations } from 'next-intl/server'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default async function Home() {
+  const t = await getTranslations('HomePage')
+
   let dishList: DishListResType['data'] = []
   try {
     const result = await dishApiRequest.list()
@@ -29,7 +32,7 @@ export default async function Home() {
         />
         <div className='z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20 text-white dark:text-white'>
           <h1 className='text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold'>
-            Nhà hàng Hafo
+            {t('title')}
           </h1>
           <p className='text-center text-sm sm:text-base mt-4'>
             Vị ngon, trọn khoảnh khắc
