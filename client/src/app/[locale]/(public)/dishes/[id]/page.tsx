ec611e4cac50c3c0f34e7fef5d3bb10f8b85f670
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-08-06 14:54:24 +0700

    update: Setup i18n routing

---
+++ client/src/app/[locale]/(public)/dishes/[id]/page.tsx
@@ -0,0 +1,17 @@
+import dishApiRequest from '@/apiRequests/dish'
+import DishDetail from '@/app/[locale]/(public)/dishes/[id]/dish-detail'
+import { formatCurrency, wrapServerApi } from '@/lib/utils'
+import Image from 'next/image'
+
+export default async function DishPage({
+  params: { id },
+}: {
+  params: {
+    id: string
+  }
+}) {
+  const data = await wrapServerApi(() => dishApiRequest.getDish(Number(id)))
+
+  const dish = data?.payload?.data
+  return <DishDetail dish={dish} />
+}
--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,10 +1,11 @@
-import { getUserLocale } from '@/services/locale'
+import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
+import { locales } from '@/config'
 
-export default getRequestConfig(async () => {
-  const locale = await getUserLocale()
+export default getRequestConfig(async ({ locale }) => {
+  if (!locales.includes(locale as any)) notFound()
+
   return {
-    locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   }
 })
