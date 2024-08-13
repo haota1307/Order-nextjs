c472b1267cf437b3b949ed9719d361c0a164295c
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-08-13 15:13:53 +0700

    fixbug (v2): Bug chuyển ngôn ngữ

--- client/src/components/switch-language.tsx
+++ client/src/components/switch-language.tsx
@@ -4,39 +4,25 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
 import { Locale, locales } from '@/config'
 import { useLocale, useTranslations } from 'next-intl'
-import { useSearchParams, useParams } from 'next/navigation'
-import { usePathname, useRouter } from 'next/navigation'
-import { Suspense } from 'react'
+import { usePathname, useRouter } from '@/navigation'
 
 export default function SwitchLanguage() {
-  return (
-    <Suspense>
-      <SwitchLanguageMain />
-    </Suspense>
-  )
-}
-
-function SwitchLanguageMain() {
   const t = useTranslations('SwitchLanguage')
   const locale = useLocale()
   const pathname = usePathname()
-  const params = useParams()
-  const searchParams = useSearchParams()
   const router = useRouter()
   return (
     <Select
       value={locale}
       onValueChange={(value) => {
-        const locale = params.locale as Locale
-        const newPathname = pathname.replace(`/${locale}`, `/${value}`)
-        const fullUrl = `${newPathname}?${searchParams.toString()}`
-        router.replace(fullUrl)
+        router.replace(pathname, {
+          locale: value as Locale,
+        })
         router.refresh()
       }}
     >
